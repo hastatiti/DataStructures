a41f3f75567474b6b232e56017e7f958ae0c21f1
ady <ady@adys-macbook-pro.local>
2018-11-07 13:00:27 +0000

    Refactor Ex11

--- DataStructures/src/Chapter1Fundamentals/BagsQueuesStacks/Ex11.java
+++ DataStructures/src/Chapter1Fundamentals/BagsQueuesStacks/Ex11.java
@@ -3,34 +3,36 @@ package Chapter1Fundamentals.BagsQueuesStacks;
 import java.util.Stack;
 
 public class Ex11 {
-	static Stack<String> stack = new Stack<String>();
-
+	static Stack<String> ops = new Stack<String>();
+	static Stack<String> vals = new Stack<String>();
+	
 	public static String evaluate(String str) {
 		for(String s : str.split(" ")) {
 			if(s.equals("("))
-			{
-			}
+			{}
+			else if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/"))
+				ops.push(s);
 			else if(s.equals(")")) {
-				String a = stack.pop();
-				String b = stack.pop();
-				String c = stack.pop();
+				String operator = ops.pop();
+				String b = vals.pop();
+				String a = vals.pop();
 				double no1 = Double.parseDouble(b);
-				double no2 = Double.parseDouble(c);
+				double no2 = Double.parseDouble(a);
 			
 				double together = 0.0;
-				if(a.equals("+"))
+				if(operator.equals("+"))
 					together = no1 + no2 ;  
-				if(a.equals("-"))
+				if(operator.equals("-"))
 					together = no2 - no1 ;  
-				if(a.equals("*"))
+				if(operator.equals("*"))
 					together = no1 * no2 ;  
 				String total = Double.toString(together);
-				stack.push(total);
+				vals.push(total);
 			}
-			else stack.push(s);
+			else vals.push(s);
 	}
 		//It is just one value returned by String together
-		return stack.pop();
+		return vals.pop();
 	}
 	public static void main(String[] args) {
 		String str = "( ( 1 2 + ) ( ( 6 4 - ) ( 8 6 - ) * ) * )";
